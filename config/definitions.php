c653417ef6df1015fef5f1d189cc6cee3f04e392
leep perk <2lperkins@gmail.com>
2019-08-22 16:25:58 -0700

    working on login

--- config/definitions.php
+++ config/definitions.php
@@ -3,16 +3,16 @@
 /*******************
  * Database Config *
  *******************/
-deifne("DB_HOST", "127.0.0.1");
+define("DB_HOST", "127.0.0.1:3306");
 define("DB_USERNAME", "root");
 define("DB_PASSWORD", "0Bdragon8712`");
-define("DB_SCHEME", "csci-130-project1");
+define("DB_SCHEME", "csci-130_project1");
 
 /*****************
  * Login Options *
  *****************/
 define("LOGIN_ALLOWED", true);      // if login system is on
-define("LOGIN_BRUTE_HOUR", 2);      // number of hours to wait if brute force
+define("LOGIN_BRUTE_HOURS", 2);      // number of hours to wait if brute force
 define("LOGIN_BRUTE_ATTMEMPT", 5);  // number of tries before account lock (0 for off)
 
 /******************
@@ -20,5 +20,10 @@ define("LOGIN_BRUTE_ATTMEMPT", 5);  // number of tries before account lock (0 fo
  ******************/
 define("SESSION_LIFE", 0);          // lifetime of the session (hrs)
 define("SESSION_SECURE", false);    // wether the session is secure or not (false for debug)
+
+/***************
+ * Enum String *
+ ***************/
+define("ENUM_GENDER", ['boy', 'girl', 'other']);    // Gender: enum to string
 
 ?>
