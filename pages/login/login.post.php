14da0e63076dc8a53a76491aaafd45e527149bfe
Lee Perkins <2lperkins@gmail.com>
2019-08-22 23:13:33 -0700

    updated login and register classes, added forms to login and register html.

--- pages/login/login.post.php
+++ pages/login/login.post.php
@@ -1,20 +1,32 @@
 <?php
 include_once $_SERVER["DOCUMENT_ROOT"] . "/php/classes/login.class.php";
-echo "HERE";
+
 /**
  * POST:
  *  string username
  *  string password
+ * 
+ * processes POST data and attempts to log user in.
+ * @return json {"result": booelan, "error"?: string}
  */
-$err = "";
-$Login = new Login();
-if ($Login->login_check()) {
-    if (isset($_POST['username'], $_POST['password'])) {
-        return $Login->run_login($_POST['username'], $_POST['password']);
-    } else {
-        $err .= "Username or Password was empty!";
-        return false;
-    }
-}
 
+$data = [];
+$data['error'] = '';
+
+$Login = new Login();
+if (!$Login->login_check()) {
+    if (isset($_POST['username'], $_POST['password'])) {
+        if(!$Login->run_login($_POST['username'], $_POST['password'])) {
+            $data['error'] .= $Login->error;
+            $data['result'] = false;
+        } else 
+            $data['result'] = true;
+    } else {
+        $data['error'] .= "Username or Password was empty!";
+        $data['result'] = false;
+    }
+} else {
+    $data['result'] = true;
+}
+echo json_encode($data);
 ?>
