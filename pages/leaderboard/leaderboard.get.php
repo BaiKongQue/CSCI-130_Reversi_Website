e16ebb95a3ee9a90637f24aa16d9ae59c365807a
leep perk <2lperkins@gmail.com>
2019-08-27 17:06:31 -0700

    added more to game class, added comments, added to nav

--- pages/leaderboard/leaderboard.get.php
+++ pages/leaderboard/leaderboard.get.php
@@ -1,7 +1,18 @@
 <?php
 include_once $_SERVER['DOCUMENT_ROOT'] . '/php/classes/game.class.php';
 
-$Game = new Game();
-return $Game->get_game_data();
+/**
+ * GET:
+ *  player_id
+ *  sort
+ *  order
+ */
 
+$Game = new Game();
+echo json_encode($Game->get_scores(
+    (isset($_GET['first_name'])) ? $_GET['first_name'] : NULL,
+    (isset($_GET['last_name'])) ? $_GET['last_name'] : NULL,
+    (isset($_GET['sort'])) ? $_GET['sort'] : 'score',
+    (isset($_GET['order'])) ? $_GET['order'] : 'DESC'
+));
 ?>
