e9c6a626b5890e4f3d95dc990cf0bbcad7c466f9
leep perk <2lperkins@gmail.com>
2019-09-11 12:55:22 -0700

    added comments to move check function and files

--- pages/game/move.check.php
+++ pages/game/move.check.php
@@ -1,12 +1,19 @@
 <?php
 include_once $_SERVER['DOCUMENT_ROOT'] . "/php/classes/game.class.php";
 
-$res = [];
-$Game = new Game();
-$data = $Game->get_game_data($_GET['id']);
-
-$res['result'] = $Game->moves_array($data);
-if (!empty($Game->error))
-    $res['error'] = $Game->error;
-echo json_encode($res)
+/**
+ * GET:
+ *  id: int
+ * @param int id: id of the game to process
+ * @return array {[key: int]: int}: where the key is the index and value is the number of tiles
+ *  you will get in that spot
+ */
+ 
+$res = [];                                      // init res
+$Game = new Game();                             // new Game object
+$data = $Game->get_game_data($_GET['id']);      // get game data
+$res['result'] = $Game->moves_array($data);     // store result of moves
+if (!empty($Game->error))                       // if error
+    $res['error'] = $Game->error;               // record error
+echo json_encode($res)                          // return json encoded res
 ?>
