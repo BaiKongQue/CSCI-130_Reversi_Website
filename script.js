85c7c9dbd04cfdcceb743785765341d2145d9c25
leep perk <2lperkins@gmail.com>
2019-09-11 19:58:13 -0700

    adding locking check

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
 /*********************
  * Global Javascript *
  *********************/
+var urlBase = "127.0.0.1";
+
+function is_logged_in() {
+    var xhttp = (window.XMLHttpRequest) ? new XMLHttpRequest() : new ActiveXObject("Microsoft.XMLHTTP");
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            if (!JSON.parse(this.responseText)['result']) {
+                document.location = urlBase;
+            }
+        } else {
+            document.location = urlBase;
+        }
+    }
+    xhttp.open("GET", "./php/login.check.php", true);
+    xhttp.send();
+}
