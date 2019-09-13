1b873ff1f296cb6ff04185328a064035a50c1235
leep perk <2lperkins@gmail.com>
2019-09-12 17:31:51 -0700

    changes to is logged in function

--- script.js
+++ script.js
@@ -1,19 +1,14 @@
 /*********************
  * Global Javascript *
  *********************/
-var urlBase = "127.0.0.1";
 
-function is_logged_in() {
-    var xhttp = (window.XMLHttpRequest) ? new XMLHttpRequest() : new ActiveXObject("Microsoft.XMLHTTP");
+function is_logged_in(callback) {
+    let xhttp = (window.XMLHttpRequest) ? new XMLHttpRequest() : new ActiveXObject("Microsoft.XMLHTTP");
     xhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
-            if (!JSON.parse(this.responseText)['result']) {
-                document.location = urlBase;
-            }
-        } else {
-            document.location = urlBase;
+            callback(JSON.parse(this.responseText)['result']);
         }
     }
-    xhttp.open("GET", "./php/login.check.php", true);
+    xhttp.open("GET", "http://"+location.hostname+"/php/login.check.php", true);
     xhttp.send();
 }
