4cc9c316bceeccd1af3b43aaee144d8c3840edfa
Lee Perkins <2lperkins@gmail.com>
2019-09-15 16:19:48 -0700

    adjusted game structure, rendering and loop now work within closure environment

--- pages/game/game.js
+++ pages/game/game.js
@@ -1,64 +1,63 @@
-class Board {
-    constructor() {
-        this.canvas = document.getElementById("board");
-        this.context = this.canvas.getContext("2d");
-        this.data = {};
-        this.start = null;
-        this.grid_size = 0;
-        this.bit_size = 0;
-    }
+var canvas = document.getElementById("board");
+var context = canvas.getContext("2d");
+var data = {};
+var grid_size = 0;
+var bit_size = 0;
+var frameRate = 60;
 
-    getRouteParams() {
-        var vars = {};
-        var parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function(m,key,value) {
-            vars[key] = value;
-        });
-        return vars;
-    }
-
-    onInit() {
-        this.canvas.width = "600";
-        this.canvas.height = "600";
-
-        var dataHttp = (window.XMLHttpRequest) ? new XMLHttpRequest() : new ActiveXObject("Microsoft.XMLHTTP");
-        dataHttp.onreadystatechange = function() {
-            if (this.readyState == 4 && this.status == 200) {
-                this.data = JSON.parse(this.responseText)['result'];
-
-                //Find the grid size and bit_size
-                let length = this.data['grid'].length;
-                this.grid_size = Math.sqrt(length);
-                this.bit_size = length / grid_size;
-            }
-        }
-
-        const game_id = this.getRouteParams()["id"];
-        if(game_id != undefined) {
-            dataHttp.open("GET", "game.get.php?id=" + game_id, true);
-            dataHttp.send();
-        }  
-        window.requestAnimationFrame((timestamp) => this.onRender(timestamp));
-    }
-
-    onRender(timestamp) {
-        if(!this.start) {
-            this.start = timestamp;
-        }
-        let progress = timestamp - this.start;
-
-        for(var i = 0; i < this.grid_size; ++i) {
-            for(var j = 0; j < this.grid_size; ++j) {
-                this.context.fillStyle = 'green';
-                this.context.fillRect((i*this.bit_size)+1, (j*this.bit_size)+1, this.bit_size-2, this.bit_size-2);
-            }
-        }
-
-        if(progress < 2000) {
-            window.requestAnimationFrame((timestamp) => this.onRender(timestamp));
-        }
-    }
-    
+// PRIVATE
+function _GetRouteParams() {
+    var vars = {};
+    var parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function (m, key, value) {
+        vars[key] = value;
+    });
+    return vars;
 }
 
+function _GetGameData() {
+    var dataHttp = (window.XMLHttpRequest) ? new XMLHttpRequest() : new ActiveXObject("Microsoft.XMLHTTP");
+    dataHttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            data = JSON.parse(this.responseText)['result'];
 
+            //Find the grid size and bit_size
+            let length = data['grid'].length;
+            grid_size = Math.sqrt(length);
+            bit_size = 600 / grid_size;
+            _OnRender();
+        }
+    }
 
+    const game_id = _GetRouteParams()["id"];
+    if (game_id != undefined) {
+        dataHttp.open("GET", "game.get.php?id=" + game_id, true);
+        dataHttp.send();
+    }
+}
+
+function _OnRender() {
+    context.fillStyle = "black";
+    context.fillRect(0, 0, canvas.width, canvas.height);
+
+    for (var i = 0; i < grid_size; ++i) {
+        for (var j = 0; j < grid_size; ++j) {
+            context.fillStyle = 'green';
+            context.fillRect((i * bit_size) + 1, (j * bit_size) + 1, bit_size - 2, bit_size - 2);
+        }
+    }
+}
+
+// PUBLIC
+function OnInit() {
+    canvas.width = 600;
+    canvas.height = 600;
+    context.font = "30px Arial";
+    context.fillStyle = "white";
+    context.textAlign = "center";
+    context.fillText("Loading...", canvas.width/2, canvas.height/2);
+
+    _GetGameData();
+    setInterval(() => {
+        _OnRender();
+    }, 1000/frameRate);
+}
