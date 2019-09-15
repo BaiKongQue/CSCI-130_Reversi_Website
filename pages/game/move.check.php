deaf06f8a4c11e7445cde681e9fdfb7c2a1ef83d
Lee Perkins <2lperkins@gmail.com>
2019-09-14 21:48:43 -0700

    revised comments

--- pages/game/move.check.php
+++ pages/game/move.check.php
@@ -5,7 +5,7 @@ include_once $_SERVER['DOCUMENT_ROOT'] . "/php/classes/game.class.php";
  * GET:
  *  id: int
  * @param int id: id of the game to process
- * @return array {[key: int]: int}: where the key is the index and value is the number of tiles
+ * @return array {result: {[key: int]: int}, error?: string}: where the key is the index and value is the number of tiles
  *  you will get in that spot
  */
  
