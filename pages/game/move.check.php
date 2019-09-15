3a3ad03379eca22a5ff17e91acc6e675d29ce690
Lee Perkins <2lperkins@gmail.com>
2019-09-14 23:05:56 -0700

    move check now takes game data as POST

--- pages/game/move.check.php
+++ pages/game/move.check.php
@@ -2,18 +2,23 @@
 include_once $_SERVER['DOCUMENT_ROOT'] . "/php/classes/game.class.php";
 
 /**
- * GET:
- *  id: int
- * @param int id: id of the game to process
+ * POST:
+ *  data: array
+ * @param array data: game data package
  * @return array {result: {[key: int]: int}, error?: string}: where the key is the index and value is the number of tiles
  *  you will get in that spot
  */
  
 $res = [];                                      // init res
-$Game = new Game();                             // new Game object
-$data = $Game->get_game_data($_GET['id']);      // get game data
-$res['result'] = $Game->moves_array($data);     // store result of moves
-if (!empty($Game->error))                       // if error
-    $res['error'] = $Game->error;               // record error
+if (isset($_POST['data']) && !empty($_POST['data'])) {
+    $data = json_decode($_POST['data']);
+    $Game = new Game();                             // new Game object
+    $res['result'] = $Game->moves_array($data);     // store result of moves
+    if (!empty($Game->error))                       // if error
+        $res['error'] = $Game->error;               // record error
+} else {
+    $res['result'] = false;
+    $res['error'] = "Game data is missing!";
+}
 echo json_encode($res)                          // return json encoded res
 ?>
