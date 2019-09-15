1a0c8f491bfe25d23377f1fdd6745e2c2799993e
Lee Perkins <2lperkins@gmail.com>
2019-09-15 02:00:56 -0700

    cleaned up nav bar

--- script.js
+++ script.js
@@ -12,3 +12,27 @@ function is_logged_in(callback) {
     xhttp.open("GET", "http://"+location.hostname+"/php/login.check.php", true);
     xhttp.send();
 }
+
+/**************
+ * Navigation *
+ **************/
+var isPop = false;
+window.addEventListener("click", closeNavFromOutside, false);
+
+function closeNavFromOutside(e) {
+    if(!document.getElementById("nav-container").contains(e.target) && 
+       !document.getElementById("nav-button-open").contains(e.target)){
+        document.getElementById("nav-container").style.left = "-300px";
+        isPop = !isPop;
+    }
+}
+
+function popNav() {
+    isPop = !isPop;
+    if(isPop) {
+        document.getElementById("nav-container").style.left = "0";
+    }
+    else {
+        document.getElementById("nav-container").style.left = "-300px";
+    }
+}
