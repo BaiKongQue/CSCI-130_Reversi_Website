3fb0a516be935bda8eb51b7466da9bc3e74ffdf0
Lee Perkins <2lperkins@gmail.com>
2019-10-22 00:09:08 -0700

    added color pick for game, added available spaces to game

--- pages/game/game.js
+++ pages/game/game.js
@@ -1,9 +1,14 @@
+var boardColor = document.getElementById("board-color");
+var p1PieceColor = document.getElementById("p1-color");
+var p2PieceColor = document.getElementById("p2-color");
+
 var canvas = document.getElementById("board");
 var context = canvas.getContext("2d");
 var data = {};
 var grid_size = 0;
 var bit_size = 0;
 var frameRate = 60;
+var available_move = {};
 
 // PRIVATE
 function _GetRouteParams() {
@@ -24,14 +29,14 @@ function _GetGameData() {
             let length = data.grid.length;
             grid_size = Math.sqrt(length);
             bit_size = 600 / grid_size;
-            _OnRender();
+            _GetMoveAvialable();
         }
     }
 
     const game_id = _GetRouteParams()["id"];
     if (game_id != undefined) {
         dataHttp.open("GET", "game.get.php?id=" + game_id, true);
-        dataHttp.send();
+        dataHttp.send("data=" + JSON.stringify(data));
     }
 }
 
@@ -56,20 +61,46 @@ function _OnRender() {
     for (var i = 0; i < grid_size; ++i) {
         for (var j = 0; j < grid_size; ++j) {
             context.beginPath()
-            context.fillStyle = 'green';
+            context.fillStyle = boardColor.value;
             context.fillRect((i * bit_size) + 1, (j * bit_size) + 1, bit_size - 2, bit_size - 2);
             context.closePath();
             
             if (data.grid[current_index] == 1) {
-                _DrawCircle('white', i, j); 
+                _DrawCircle(p1PieceColor.value, i, j); 
             } else if (data.grid[current_index] == 2) {
-                _DrawCircle('black', i, j);
+                _DrawCircle(p2PieceColor.value, i, j);
+            }
+
+            if (current_index in available_move) {
+                context.beginPath()
+                context.fillStyle = 'pink';
+                context.fillRect((i * bit_size) + 1, (j * bit_size) + 1, bit_size - 2, bit_size - 2);
+                context.closePath();
+
+                context.beginPath();
+                context.fillStyle = 'white';
+                context.fillText(available_move[current_index], (i * bit_size) + bit_size/2 + 1, (j * bit_size) + bit_size/2 + 12);
+                context.closePath();
+
             }
             current_index++;
         }
     }
 }
 
+function _GetMoveAvialable() {
+    var dataHttp = (window.XMLHttpRequest) ? new XMLHttpRequest() : new ActiveXObject("Microsoft.XMLHTTP");
+    dataHttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            available_move = JSON.parse(this.responseText)['result'];
+            
+        }
+    }
+
+    dataHttp.open("POST", "moves.get.php", true);
+    dataHttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    dataHttp.send("data=" + JSON.stringify(data));
+}
 
 // PUBLIC
 function OnInit() {
@@ -81,6 +112,11 @@ function OnInit() {
     context.fillText("Loading...", canvas.width/2, canvas.height/2);
 
     _GetGameData();
+    setInterval(() => {
+        _GetMoveAvialable();
+    }, 5 * 1000);
+    // set inter (3 * 1000)
+    //      OnLoop
     setInterval(() => {
         _OnRender();
     }, 1000/frameRate);
