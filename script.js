a833713341a8811775a03cea43447edd5a9ad43a
Lee Perkins <2lperkins@gmail.com>
2019-11-07 14:41:39 -0800

    changed how login check works, added pipeline functionality

--- script.js
+++ script.js
@@ -5,26 +5,26 @@
  * Runs when the window finishes loading.
  */
 window.onload = function() {
-    load_login_elements();  // load the logged-in logged-out elements
+    is_logged_in();  // load the logged-in logged-out elements
 }
 
 /*********
  * Login * 
  *********/
 sessionData = [];
+login_pipe = [];
 /**
  * Checks wether the user is logged in or not by sending a request to the server.
  * @param function(bool) callback: function to run when the get request finishes, and gives a parameter if the user is logged in or not
  */
-function is_logged_in(callback) {
+function is_logged_in(callback = () => {}) {
     let xhttp = new XMLHttpRequest();                                               // create new xmlhttp request 
     xhttp.onreadystatechange = function() {                                         // if the state changes and finishes
         if (this.readyState == 4 && this.status == 200) {                           // successfully get
             let res = JSON.parse(this.responseText);                                // get result
-            if (res.result) {
-                sessionData = res.session;                                          // set session data
-                callback(res.result);                                               // callback function and pass result
-            }
+            sessionData = res.result.session;                                       // set session data
+            for (let p of login_pipe) p(res.result);                                // send result to piped functions
+            callback(res.result);                                                   // callback function and pass result
         }
     }
     xhttp.open("GET", "http://"+location.hostname+"/php/login.check.php", true);    // prepare get request
@@ -35,23 +35,22 @@ function is_logged_in(callback) {
  * shows or hides all the elements with the class logged-in or logged-out according to if the user
  * is logged in or out.
  */
-function load_login_elements() {
+function load_login_elements(loggedIn) {
     var loggedInElements = document.getElementsByClassName("logged-in");        // get all logged-in classes
     var loggedOutElements = document.getElementsByClassName("logged-out");      // get all logged-out classes
-    this.is_logged_in(function(loggedIn) {                                      // get if the user is logged in or not
-        if (loggedIn) {                                                         // if logged in
-            for (let c of loggedInElements)                                     // for each logged-in element
-                c.style.display = "inherit";                                    // show
-            for (let c of loggedOutElements)                                    // for each logged-out element
-                c.style.display = "none";                                       // hide
-        } else {
-            for (let c of loggedInElements)                                     // for each logged-in element
-                c.style.display = "none";                                       // hide
-            for (let c of loggedOutElements)                                    // for each logged-out element
-                c.style.display = "inherit";                                    // show
-        }
-    });
+    if (loggedIn) {                                                         // if logged in
+        for (let c of loggedInElements)                                     // for each logged-in element
+            c.style.display = "inherit";                                    // show
+        for (let c of loggedOutElements)                                    // for each logged-out element
+            c.style.display = "none";                                       // hide
+    } else {
+        for (let c of loggedInElements)                                     // for each logged-in element
+            c.style.display = "none";                                       // hide
+        for (let c of loggedOutElements)                                    // for each logged-out element
+            c.style.display = "inherit";                                    // show
+    }
 }
+login_pipe.push(load_login_elements);
 
 /**************
  * Navigation *
