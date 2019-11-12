e89dc5f4a82e3a6458c4c46e0f7d7c297e4c429d
Lee Perkins <2lperkins@gmail.com>
2019-11-12 10:58:44 -0800

    made temp dummy logged in, working on lobbies join, working on game post

--- pages/game/game.post.php
+++ pages/game/game.post.php
@@ -7,7 +7,14 @@ include_once $_SERVER['DOCUMENT_ROOT'] . "/php/classes/game.class.php";
  * @param array data: package of data
  */
 $data = [];
-if (isset($_POST($data)) && empty($_POST($data))) {
-    
+if (isset($_POST['data'], $_POST['index']) && !empty($_POST['data']) && !empty($_POST['index'])) {
+    $Game = new Game();
+    $data['result'] = $Game->update_game_data($_POST['data'], $_POST['index']);
+    if ($Game->error != "") $data['error'] = $Game->error;
+} else {
+    $data['error'] = "one or more parameter is missing!";
+    $data['result'] = false;
 }
+
+echo json_encode($data);
 ?>
