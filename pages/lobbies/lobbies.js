149d09b7e193b576f93d813a18889911af63bce4
Lee Perkins <2lperkins@gmail.com>
2019-11-20 11:32:51 -0800

    fixed and cleaned up join button

--- pages/lobbies/lobbies.js
+++ pages/lobbies/lobbies.js
@@ -1,6 +1,7 @@
 let form = document.getElementById('filters-form');
 let lobbies = document.getElementById('lobbies-list-ul');
 let getLobbies = new XMLHttpRequest();
+let joinHttp = new XMLHttpRequest();
 let data = [];
 
 form.oninput = function () {
@@ -23,47 +24,52 @@ getLobbies.onreadystatechange = function () {
     }
 }
 
+joinHttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+        let res = JSON.parse(this.responseText);
+        if (res.result) {
+            document.location = "http://" + location.hostname + "/pages/game/game.html?id=" + res.game_id;
+        } else {
+            document.getElementById("error-msg").innerText = res.error;
+        }
+    }
+}
+
 function GetLobbies() {
     getLobbies.open('GET', "./lobbies.get.php", true);
     getLobbies.send();
 }
 
-function player_block(player) {
+function player_block(player, gameId, samePlayer = false) {
     return (player.first_name != null) ? "<div class=\"player-block\">" +
         "<div><h2>" + player.first_name + " " + player.last_name + "</h2></div>" +
         "<div><img src=\"../../images/upload/users/" + player.icon + "\" alt=\"player icon\" /></div>" +
         "<div><strong>Score: " + player.score + "</strong></div>" +
-        "<button id=\"viewGame\">View</button>" + viewGame() +
         "</div>"
         :
         "<div>" +
         "<div class=\"player-block\">Waiting for opponent</div>" +
-        "<button id=\"joinGame\">Join</button>" + joinGame(player.player_id) +
+        (!samePlayer ? "<button id=\"joinGame\" onclick=\"joinGame("+gameId+")\" style=\"display:block; margin: 0 auto\">Join</button>" : "") +
         "</div>";
 }
 
-function viewGame() {
-    console.log("hello world");
-}
-
-function joinGame($playerid) {
-    console.log("join the game: ", sessionData.player_id);
-    getLobbies.open('POST', "./join.post.php", true);
-    getLobbies.send("game_id=" + $gameId + "&player1_id=" + $playerid);
+function joinGame($gameId) {
+    joinHttp.open('POST', "./join.post.php", true);
+    joinHttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    joinHttp.send("game_id=" + $gameId + "&player_id=" + sessionData.player_id);
 }
 
 function DisplayData(r) {
     lobbies.innerHTML += "<li>" +
-        "<a href=\"../game/game.html?id=" + r.game_id + "\">" +
         "<div id=\"player-blocks\">" +
-        player_block(r.player1) +
-        "<div><h1>VS</h1></div>" +
-        player_block(r.player2) +
+            player_block(r.player1, r.game_id) +
+            "<div><h1>VS</h1></div>" +
+            player_block(r.player2, r.game_id, (r.player1.id == sessionData.player_id)) +
         "</div>" +
+        "<div><a href=\"../game/game.html?id=" + r.game_id + "\"><button style=\"display:block;margin:0 auto\">View Game</button></a></div>" +
         "<div id=\"game-duration\">Duration: " + r.duration + "</div>" +
         "<div id=\"game-id\">Game id: " + r.game_id + "</div>" +
-        "</a>" +
-        "</li>";
+    "</li>";
 }
 
 function LoadData() {
