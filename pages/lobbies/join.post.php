bf46e45a51e7fd66e6cefba167f4df6c60b5daa3
PumpkinKing93 <tgillette@mail.fresnostate.edu>
2019-11-20 07:29:56 -0800

    working on join game

--- pages/lobbies/join.post.php
+++ pages/lobbies/join.post.php
@@ -3,7 +3,7 @@ include_once $_SERVER['DOCUMENT_ROOT'] . "/php/classes/game.class.php";
 
 if(isset($_POST['game_id'], $_POST['player1_id']) && !empty($_POST['player_id'])){
     if($_POST['player1_id'] != $_SESSION['player_id']){
-        
+        join_game($_POST['game_id']);
     }
 }
   
