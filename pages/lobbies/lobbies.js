401b846f3c414ef20c665c14079078a9008620c2
Lee Perkins <2lperkins@gmail.com>
2019-11-22 22:12:27 -0800

    fixed lobbies if there are now now displays message

--- pages/lobbies/lobbies.js
+++ pages/lobbies/lobbies.js
@@ -12,16 +12,12 @@ getLobbies.onreadystatechange = function () {
     if (this.readyState == 4 && this.status == 200) {
         let res = JSON.parse(this.responseText);
         if (res.result) {
-            if (res.result.length <= 0) {
-                lobbies.innerHTML += "<div style=\"text-align:center\">You have no current games! Go start one by <a href=\"../create-game/create-game.html\">Creating a game!</a></div>"
+            data = res.result;
+            if (!sessionData.player_id) {
+                login_pipe.push(LoadData);
             } else {
-                data = res.result;
-                if (!sessionData.player_id) {
-                    login_pipe.push(LoadData);
-                } else {
-                    LoadData();
+                LoadData();
                 }
-            }
         } else {
             document.getElementById("error-msg").innerText = res.error;
         }
@@ -92,16 +88,31 @@ function DisplayData(r) {
     "</li>";
 }
 
+function exists() {
+    return data.some(function(i) {
+        return i.id == sessionData.player_id;
+    });
+}
+
 function LoadData() {
     let formData = new FormData(form);
-    let matches = (name, data) => formData.get(name) != "" && !data.toLowerCase().includes(formData.get(name).toLowerCase());
+    let matches = (name, data) => {
+        console.log(name, data, formData.get(name));
+        return formData.get(name) != "" && !data.toLowerCase().includes(formData.get(name).toLowerCase())
+    };
+
+    if (!exists()) {
+        lobbies.innerHTML += "<div style=\"text-align:center\">You have no current games! Go start one by <a href=\"../create-game/create-game.html\">Creating a game!</a></div>"
+        return;
+    }
+
     lobbies.innerHTML = "";
 
     for (let r of data) {
         let cont = false;
         if (formData.get('view-all') || (!formData.get('view-all') && (r.player1.id == sessionData.player_id || (r.player2.id && r.player2.id == sessionData.player_id)))) {
             for (let i of ['first_name', 'last_name']) {
-                if (matches(i, r.player1[i]) || (r.player2.id && matches(i, r.player2[i]))) {
+                if (matches(i, r.player1[i]) || (!!r.player2[i] && matches(i, r.player2[i]))) {
                     cont = true;
                     break;
                 }
