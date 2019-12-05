500673a50a971e361654849554bb3e9206a1a998
PumpkinKing93 <93gymnast@gmail.com>
2019-12-04 19:32:13 -0800

    Added db creation to registration class php file

--- php/classes/login.class.php
+++ php/classes/login.class.php
@@ -131,6 +131,47 @@ class Login{
 		// return true;
 	}
 	
+	public function dbSetup() {
+        if ($this->Login->login_check()) {
+            if ($stmt = $this->Mysqli->prepare("
+                SELECT
+                    players.*
+				FROM
+					players
+            ")) { if ($stmt->execute()) {
+                    if($stmt == NULL){
+						$stmt1 = $this->Mysqli->prepare("
+						CREATE TABLE `players` (
+							`player_id` int(11) NOT NULL AUTO_INCREMENT,
+							`username` varchar(45) NOT NULL,
+							`password` char(60) NOT NULL,
+							`first_name` varchar(45) NOT NULL,
+							`last_name` varchar(45) NOT NULL,
+							`age` int(11) NOT NULL DEFAULT '1',
+							`gender` enum('boy','girl','other') NOT NULL DEFAULT 'other',
+							`location` varchar(60) NOT NULL,
+							`icon` varchar(45) NOT NULL,
+							PRIMARY KEY (`player_id`),
+							UNIQUE KEY `player_id_UNIQUE` (`player_id`),
+							UNIQUE KEY `username_UNIQUE` (`username`)
+						  ) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8
+						")
+
+						// $stmt1->execute();
+					}
+                }
+                return;
+            } else {
+                $this->error .= "Failed to communicate with the server!\n";
+                return false;
+            }
+        } else {
+            $this->error .= "You are not logged in!\n";
+            return false;
+        }
+    }
+
+
 	public function __destruct(){
 		$this->Mysqli->close();	// close mysql connection
 		$this->Mysqli = null; 	// clear
